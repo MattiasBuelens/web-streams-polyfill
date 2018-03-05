31c9b60eb1ebfdd49b98e26dd2d9720706ab77c8
Mattias Buelens <mattias@buelens.com>
2018-03-05 23:24:09 +0100

    Fix paths

--- src/index.es6.js
+++ src/index.es6.js
@@ -1,9 +1,9 @@
 export const
-  { ReadableStream } = require('./spec/reference-implementation/lib/readable-stream'),
-  { WritableStream } = require('./spec/reference-implementation/lib/writable-stream'),
-  ByteLengthQueuingStrategy = require('./spec/reference-implementation/lib/byte-length-queuing-strategy'),
-  CountQueuingStrategy = require('./spec/reference-implementation/lib/count-queuing-strategy'),
-  { TransformStream } = require('./spec/reference-implementation/lib/transform-stream');
+  { ReadableStream } = require('../spec/reference-implementation/lib/readable-stream'),
+  { WritableStream } = require('../spec/reference-implementation/lib/writable-stream'),
+  ByteLengthQueuingStrategy = require('../spec/reference-implementation/lib/byte-length-queuing-strategy'),
+  CountQueuingStrategy = require('../spec/reference-implementation/lib/count-queuing-strategy'),
+  { TransformStream } = require('../spec/reference-implementation/lib/transform-stream');
 
 const interfaces = {
   ReadableStream,
