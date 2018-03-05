81af804acb07b35992dccace4b012ea83e08f025
Mattias Buelens <mattias@buelens.com>
2018-03-06 00:18:51 +0100

    Set up minified Rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const rollupCommonJS = require('rollup-plugin-commonjs');
 const rollupAlias = require('rollup-plugin-alias');
+const rollupUglify = require('rollup-plugin-uglify');
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
   input: 'src/index.es6.js',
   output: [{
-    file: 'dist/polyfill.js',
+    file: PRODUCTION ? 'dist/polyfill.min.js' : 'dist/polyfill.js',
     format: 'umd',
     name: 'WebStreamsPolyfill'
   }],
@@ -17,6 +20,12 @@ module.exports = {
     rollupAlias({
       'better-assert': path.resolve(__dirname, './src/stub/better-assert.js'),
       'debug': path.resolve(__dirname, './src/stub/debug.js')
-    })
-  ]
+    }),
+    PRODUCTION ? rollupUglify({
+      keep_classnames: true, // needed for WPT
+      compress: {
+        inline: 1 // TODO re-enable when this is fixed: https://github.com/mishoo/UglifyJS2/issues/2842
+      }
+    }) : undefined
+  ].filter(Boolean)
 };
