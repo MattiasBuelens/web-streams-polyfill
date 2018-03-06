c0ee11379a6dfe7ac303eb9a18f0d411f9df74c3
Mattias Buelens <mattias@buelens.com>
2018-03-06 22:29:11 +0100

    Fix lint errors

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,8 +1,10 @@
+/* global window */
 import * as interfaces from './ponyfill';
 
 // Export
 export * from './ponyfill';
 
 // Add classes to window
-if ( typeof window !== "undefined" )
-  Object.assign( window, interfaces );
+if (typeof window !== 'undefined') {
+  Object.assign(window, interfaces);
+}
