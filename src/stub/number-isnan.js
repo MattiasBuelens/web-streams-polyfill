805cf0b9ed3cea505c4d2e41419357d75dad0e41
Mattias Buelens <mattias@buelens.com>
2018-04-19 21:49:09 +0200

    Fix lint issue

--- src/stub/number-isnan.js
+++ src/stub/number-isnan.js
@@ -1,5 +1,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN#Polyfill
 const NumberIsNaN = Number.isNaN || function (x) {
+  // eslint-disable-next-line no-self-compare
   return x !== x;
 };
 
