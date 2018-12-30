387b1d0a5e6069b2002b12816433514f98b631ff
Mattias Buelens <mattias@buelens.com>
2018-12-30 15:28:45 +0100

    Update test script to latest spec version

--- run-web-platform-tests-on-bundle.js
+++ run-web-platform-tests-on-bundle.js
@@ -1,6 +1,6 @@
 // This runs the web platform tests against the reference implementation, in Node.js using jsdom, for easier rapid
 // development of the reference implementation and the web platform tests.
-/* eslint-disable no-console*/
+/* eslint-disable no-console */
 
 const path = require('path');
 const wptRunner = require('wpt-runner');
@@ -14,15 +14,6 @@ const {
   CountQueuingStrategy
 } = require('./dist/ponyfill.es6.min');
 
-const testsPath = path.resolve(__dirname, 'spec/reference-implementation/web-platform-tests/streams');
-
-const filterGlobs = process.argv.length >= 3 ? process.argv.slice(2) : ['**/*.html'];
-const workerTestPattern = /\.(?:dedicated|shared|service)worker(?:\.https)?\.html$/;
-function filter(testPath) {
-  return !workerTestPattern.test(testPath) && // ignore the worker versions
-         filterGlobs.some(glob => minimatch(testPath, glob));
-}
-
 // wpt-runner does not yet support unhandled rejection tracking a la
 // https://github.com/w3c/testharness.js/commit/7716e2581a86dfd9405a9c00547a7504f0c7fe94
 // So we emulate it with Node.js events
@@ -35,37 +26,50 @@ process.on('rejectionHandled', promise => {
   rejections.delete(promise);
 });
 
-wptRunner(testsPath, { rootURL: 'streams/', setup, filter })
-  .then(failures => {
-    process.exitCode = failures;
+main().catch(e => {
+  console.error(e.stack);
+  process.exitCode = 1;
+});
 
-    if (rejections.size > 0) {
-      if (failures === 0) {
-        process.exitCode = 1;
-      }
+async function main() {
+  const testsPath = path.resolve(__dirname, 'spec/reference-implementation/web-platform-tests/streams');
 
-      for (const reason of rejections.values()) {
-        console.error('Unhandled promise rejection: ', reason.stack);
-      }
+  const filterGlobs = process.argv.length >= 3 ? process.argv.slice(2) : ['**/*.html'];
+  const workerTestPattern = /\.(?:dedicated|shared|service)worker(?:\.https)?\.html$/;
+
+  const failures = await wptRunner(testsPath, {
+    rootURL: 'streams/',
+    setup(window) {
+      // Necessary so that we can send test-realm promises
+      // to the jsdom-realm implementation without causing assimilation.
+      window.Promise = Promise;
+
+      // TODO Find a cleaner way to expose globals for aborting
+      global.AbortController = window.AbortController;
+      global.AbortSignal = window.AbortSignal;
+      global.DOMException = window.DOMException;
+
+      window.ReadableStream = ReadableStream;
+      window.WritableStream = WritableStream;
+      window.TransformStream = TransformStream;
+      window.ByteLengthQueuingStrategy = ByteLengthQueuingStrategy;
+      window.CountQueuingStrategy = CountQueuingStrategy;
+    },
+    filter(testPath) {
+      return !workerTestPattern.test(testPath) && // ignore the worker versions
+          filterGlobs.some(glob => minimatch(testPath, glob));
     }
-  })
-  .catch(e => {
-    console.error(e.stack);
-    process.exitCode = 1;
   });
 
-function setup(window) {
-  // Necessary so that we can send test-realm promises to the jsdom-realm implementation without causing assimilation.
-  window.Promise = Promise;
+  process.exitCode = failures;
 
-  // TODO Find a cleaner way to expose globals for aborting
-  global.AbortController = window.AbortController;
-  global.AbortSignal = window.AbortSignal;
-  global.DOMException = window.DOMException;
+  if (rejections.size > 0) {
+    if (failures === 0) {
+      process.exitCode = 1;
+    }
 
-  window.ReadableStream = ReadableStream;
-  window.WritableStream = WritableStream;
-  window.TransformStream = TransformStream;
-  window.ByteLengthQueuingStrategy = ByteLengthQueuingStrategy;
-  window.CountQueuingStrategy = CountQueuingStrategy;
+    for (const reason of rejections.values()) {
+      console.error('Unhandled promise rejection: ', reason.stack);
+    }
+  }
 }
