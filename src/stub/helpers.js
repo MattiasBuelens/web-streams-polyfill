93a46b074fe99b0f7023710844173e51bae0bdf0
Mattias Buelens <mattias@buelens.com>
2018-12-30 17:36:35 +0100

    Do not copy ArrayBuffer in TransferArrayBuffer
    Fixes #3

--- src/stub/helpers.js
+++ src/stub/helpers.js
@@ -9,11 +9,19 @@ export {
   CreateAlgorithmFromUnderlyingMethod,
   InvokeOrNoop,
   PromiseCall,
-  TransferArrayBuffer,
-  IsDetachedBuffer,
   ValidateAndNormalizeHighWaterMark,
   MakeSizeAlgorithmFromSizeFunction,
   PerformPromiseThen,
   WaitForAll,
   WaitForAllPromise
 } from '../../spec/reference-implementation/lib/helpers';
+
+// Not implemented correctly
+export function TransferArrayBuffer(O) {
+  return O;
+}
+
+// Not implemented correctly
+export function IsDetachedBuffer(O) { // eslint-disable-line no-unused-vars
+  return false;
+}
