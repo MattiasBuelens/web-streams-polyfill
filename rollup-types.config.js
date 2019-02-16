cf709717f4abcf9c03c0d93d65ebee13a208682a
Mattias Buelens <mattias@buelens.com>
2019-02-16 20:13:06 +0100

    Add banner comment to bundled types

--- rollup-types.config.js
+++ rollup-types.config.js
@@ -1,10 +1,17 @@
+const pkg = require('./package.json');
 const rollupDts = require('rollup-plugin-dts');
+
+const banner = `
+/**
+ * Type definitions for ${pkg.name} v${pkg.version}
+ */`.trim();
 
 module.exports = {
   input: 'src/polyfill.ts',
   output: {
     file: 'dist/types/polyfill.d.ts',
-    format: 'es'
+    format: 'es',
+    banner
   },
   plugins: [
     rollupDts.dts({
