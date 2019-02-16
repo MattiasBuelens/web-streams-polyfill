d1fa99b361bc1dc8841f6892ad764332f653c136
Mattias Buelens <mattias@buelens.com>
2019-02-16 22:49:35 +0100

    Tiny fix

--- src/stub/assert.ts
+++ src/stub/assert.ts
@@ -1,7 +1,7 @@
 import { noop } from '../utils';
 
 export default function assert(test: boolean): void { // eslint-disable-line @typescript-eslint/no-unused-vars
-  // do nothing
+  return; // do nothing
 }
 
 assert.AssertionError = noop;
