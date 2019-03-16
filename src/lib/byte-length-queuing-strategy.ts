47836fbc4042361485cf05a3a1d3076d6b8e69cc
Mattias Buelens <mattias@buelens.com>
2019-03-16 18:47:40 +0100

    Optimize data properties

--- src/lib/byte-length-queuing-strategy.ts
+++ src/lib/byte-length-queuing-strategy.ts
@@ -1,11 +1,10 @@
-import { createDataProperty } from './helpers';
 import { QueuingStrategy } from './queuing-strategy';
 
 export default class ByteLengthQueuingStrategy implements QueuingStrategy<ArrayBufferView> {
   readonly highWaterMark!: number;
 
   constructor({ highWaterMark }: { highWaterMark: number }) {
-    createDataProperty(this, 'highWaterMark', highWaterMark);
+    this.highWaterMark = highWaterMark;
   }
 
   size(chunk: ArrayBufferView): number {
