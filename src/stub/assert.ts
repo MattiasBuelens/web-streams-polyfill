505f727bac809bbd6e40ccae15f76b82071bf96f
Mattias Buelens <mattias@buelens.com>
2019-03-23 23:42:40 +0100

    Fix assert

--- src/stub/assert.ts
+++ src/stub/assert.ts
@@ -1,17 +1,17 @@
 /* global DEBUG */
 import { noop } from '../utils';
 
-export const AssertionError = /*@__PURE__*/ class AssertionError extends Error {
+export const AssertionError = /* @__PURE__*/ class AssertionError extends Error {
   constructor(message: string) {
     super(message);
-    Object.setPrototypeOf(this, this.constructor);
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 };
 
 const assert: (test: boolean, message?: string) => void =
   DEBUG ? (test, message) => {
     if (!test) {
-      throw new AssertionError(`Assertion failed: ${message}`);
+      throw new AssertionError('Assertion failed' + (message ? `: ${message}` : ''));
     }
   } : noop;
 
