b09868966f56b43178c5fc578c40fdf746299109
Mattias Buelens <mattias@buelens.com>
2019-11-22 18:41:53 +0100

    Fix setup for downlevel script

--- build/downlevel-dts.js
+++ build/downlevel-dts.js
@@ -1,19 +1,20 @@
-// Original from downlevel-dts (MIT licensed) by Nathan Shively-Sanders
+// Based on downlevel-dts (MIT licensed) by Nathan Shively-Sanders
 // https://github.com/sandersn/downlevel-dts/blob/e7d1cb5aced5686826fe8aac4d4af2f745a9ef60/index.js
 
 const { Project, ts } = require('ts-morph');
-const { cwd } = require('process');
 const path = require('path');
-const src = process.argv[2];
-const target = process.argv[3];
-if (!src || !target) {
-  console.log('Usage: node ../index.js . ts3.4');
-  process.exit(1);
-}
-const project = new Project({
-  tsConfigFilePath: path.join(src, 'tsconfig.json')
-});
-for (const f of project.getSourceFiles('**/*.d.ts')) {
+
+const project = new Project();
+const inputDir = project.addDirectoryAtPath(path.join(__dirname, '../dist/types/'));
+
+// Create output directory
+const outputDir = inputDir.createDirectory('ts3.4');
+project.saveSync();
+
+// Down-level all *.d.ts files in input directory
+const files = inputDir.addSourceFilesAtPaths('*.d.ts');
+for (const f of files) {
+  // Replace get/set accessors with (read-only) properties
   const gs = f.getDescendantsOfKind(ts.SyntaxKind.GetAccessor);
   for (const g of gs) {
     const s = g.getParent().getChildrenOfKind(ts.SyntaxKind.SetAccessor).find(s => s.getName() === g.getName());
@@ -29,7 +30,6 @@ for (const f of project.getSourceFiles('**/*.d.ts')) {
       s.replaceWithText(`${s.getName()}: ${s.getType().getText()}`);
     }
   }
-  f.copy(path.join(cwd(), target, path.relative(cwd(), f.getFilePath())), { overwrite: true });
-  f.refreshFromFileSystemSync();
+  f.copyToDirectory(outputDir, { overwrite: true });
 }
-project.save();
+project.saveSync();
