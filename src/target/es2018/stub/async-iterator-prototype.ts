d035366b4033fd0c8af092d3a6938bd9c6d5974d
Mattias Buelens <mattias@buelens.com>
2019-12-10 17:41:27 +0100

    Update typescript-eslint

--- src/target/es2018/stub/async-iterator-prototype.ts
+++ src/target/es2018/stub/async-iterator-prototype.ts
@@ -1,4 +1,5 @@
 /// <reference lib="es2018.asynciterable" />
 
+/* eslint-disable @typescript-eslint/no-empty-function */
 export const AsyncIteratorPrototype: AsyncIterable<any> | undefined =
   Object.getPrototypeOf(Object.getPrototypeOf(async function* (): AsyncIterableIterator<any> {}).prototype);
