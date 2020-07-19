bf6378734d9c83bd659cf66eb2ea2cdaedff9989
Mattias Buelens <mattias@buelens.com>
2020-07-19 17:55:34 +0200

    Tweak

--- src/lib/validators/pipe-options.ts
+++ src/lib/validators/pipe-options.ts
@@ -9,17 +9,19 @@ export function convertPipeOptions(options: PipeOptions | undefined,
   const preventCancel = options?.preventCancel;
   const preventClose = options?.preventClose;
   const signal = options?.signal;
+  if (signal !== undefined) {
+    assertAbortSignal(signal, `${context} has member 'signal' that`);
+  }
   return {
     preventAbort: Boolean(preventAbort),
     preventCancel: Boolean(preventCancel),
     preventClose: Boolean(preventClose),
-    signal: signal === undefined ? undefined : convertAbortSignal(signal, `${context} has member 'signal' that`)
+    signal
   };
 }
 
-function convertAbortSignal(signal: AbortSignal, context: string): AbortSignal {
+function assertAbortSignal(signal: unknown, context: string): asserts signal is AbortSignal {
   if (!isAbortSignal(signal)) {
     throw new TypeError(`${context} is not an AbortSignal.`);
   }
-  return signal;
 }
