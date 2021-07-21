1c7152425cf98cbc1c537b9bcd49a56e737fbf9b
Mattias Buelens <mattias@buelens.com>
2021-07-21 18:18:25 +0200

    Remove unminified polyfill variants

--- test/run-web-platform-tests.js
+++ test/run-web-platform-tests.js
@@ -104,18 +104,9 @@ async function main() {
 
   const results = [];
 
-  results.push(await runTests('polyfill.es6.js', {
-    excludedTests,
-    ignoredFailures
-  }));
   results.push(await runTests('polyfill.es6.min.js', {
     excludedTests,
     ignoredFailures: merge(ignoredFailures, ignoredFailuresMinified)
-  }));
-
-  results.push(await runTests('polyfill.js', {
-    excludedTests,
-    ignoredFailures: ignoredFailuresES5
   }));
   results.push(await runTests('polyfill.min.js', {
     excludedTests,
