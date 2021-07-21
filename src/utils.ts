c2198a72674c56e728892e141631feed958ed7d5
Mattias Buelens <mattias@buelens.com>
2021-07-21 22:57:00 +0200

    Move globals to separate file

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,3 @@
-/// <reference lib="dom" />
-
 export function noop(): undefined {
   return undefined;
 }
-
-function getGlobals() {
-  if (typeof self !== 'undefined') {
-    return self;
-  } else if (typeof window !== 'undefined') {
-    return window;
-  } else if (typeof global !== 'undefined') {
-    return global;
-  }
-  return undefined;
-}
-
-export const globals = getGlobals();
