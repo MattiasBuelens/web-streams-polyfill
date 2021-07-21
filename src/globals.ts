4f8b8c729e57c6a7d7000b60264b1f6882999986
Mattias Buelens <mattias@buelens.com>
2021-07-21 22:57:00 +0200

    Move globals to separate file

---
+++ src/globals.ts
@@ -0,0 +1,14 @@
+/// <reference lib="dom" />
+
+function getGlobals() {
+  if (typeof self !== 'undefined') {
+    return self;
+  } else if (typeof window !== 'undefined') {
+    return window;
+  } else if (typeof global !== 'undefined') {
+    return global;
+  }
+  return undefined;
+}
+
+export const globals = getGlobals();
