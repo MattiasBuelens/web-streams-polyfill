0172015b6b84ec03dd8950cc80bed161706652ff
Mattias Buelens <mattias@buelens.com>
2021-08-16 23:43:01 +0200

    Add bundler tests for Rollup and Webpack

---
+++ test/webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  }
+};
