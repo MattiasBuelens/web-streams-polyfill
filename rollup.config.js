ba9afa94065ebcd646d66a3013f843a9a8dd0483
Mattias Buelens <mattias@buelens.com>
2021-08-16 22:54:16 +0200

    Minify both UMD and ESM bundles

--- rollup.config.js
+++ rollup.config.js
@@ -56,7 +56,7 @@ function esm({ target = 'es5' } = {}) {
         }
       }
     ],
-    plugins: plugins({ target, minify: false })
+    plugins: plugins({ target })
   };
 }
 
@@ -73,11 +73,11 @@ function umd({ target = 'es5' } = {}) {
         freeze: false
       }
     ],
-    plugins: plugins({ target, minify: true })
+    plugins: plugins({ target })
   }));
 }
 
-function plugins({ target, minify }) {
+function plugins({ target }) {
   return [
     typescript({
       tsconfig: `tsconfig${target === 'es5' ? '' : `-${target}`}.json`,
@@ -102,7 +102,7 @@ function plugins({ target, minify }) {
       include: 'src/**/*.ts',
       functions: ['assert']
     }) : undefined,
-    minify ? terser({
+    !debug ? terser({
       keep_classnames: keepRegex, // needed for WPT
       keep_fnames: keepRegex,
       mangle: {
