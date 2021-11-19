883345301be7bb1360fa7f4a6b3e7fb8155ec3d6
Mattias Buelens <mattias@buelens.com>
2021-11-19 18:58:47 +0100

    Add tests for fallback package exports

--- test/unit/exports.spec.js
+++ test/unit/exports.spec.js
@@ -28,6 +28,36 @@ describe('package exports', () => {
   });
 });
 
+describe('fallback package exports', () => {
+  let oldGlobalReadableStream;
+  beforeEach(() => {
+    oldGlobalReadableStream = global.ReadableStream;
+  });
+  afterEach(() => {
+    global.ReadableStream = oldGlobalReadableStream;
+  });
+  it('main export works', () => {
+    const polyfill = requireUncached('../../');
+    expect(polyfill.ReadableStream).toBeDefined();
+    expect(global.ReadableStream).toBe(oldGlobalReadableStream);
+  });
+  it('es5 export works', () => {
+    const polyfill = requireUncached('../../es5');
+    expect(polyfill.ReadableStream).toBeDefined();
+    expect(global.ReadableStream).toBe(oldGlobalReadableStream);
+  });
+  it('polyfill export works', () => {
+    global.ReadableStream = undefined;
+    requireUncached('../../polyfill');
+    expect(global.ReadableStream).toBeDefined();
+  });
+  it('polyfill/es5 export works', () => {
+    global.ReadableStream = undefined;
+    requireUncached('../../polyfill/es5');
+    expect(global.ReadableStream).toBeDefined();
+  });
+});
+
 function requireUncached(module) {
   delete require.cache[require.resolve(module)];
   // eslint-disable-next-line global-require
