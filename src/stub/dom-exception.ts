5d321d37c563131ba2fc3e26e857e62143d26faa
Mattias Buelens <mattias@buelens.com>
2022-05-24 23:35:56 +0200

    Tweak comments

--- src/stub/dom-exception.ts
+++ src/stub/dom-exception.ts
@@ -21,14 +21,23 @@ function isDOMExceptionConstructor(ctor: unknown): ctor is DOMExceptionConstruct
   }
 }
 
-// - Web browsers
-// - Node 18 and higher (https://github.com/nodejs/node/commit/e4b1fb5e6422c1ff151234bb9de792d45dd88d87)
+/**
+ * Support:
+ * - Web browsers
+ * - Node 18 and higher (https://github.com/nodejs/node/commit/e4b1fb5e6422c1ff151234bb9de792d45dd88d87)
+ */
 function getFromGlobal(): DOMExceptionConstructor | undefined {
   const ctor = globals?.DOMException;
   return isDOMExceptionConstructor(ctor) ? ctor : undefined;
 }
 
-// - Node 15 and higher (https://github.com/nodejs/node/commit/eee522ac29864a55a8bc6686e6b38e93270aa1ca)
+/**
+ * Support:
+ * - Node 15 and higher (https://github.com/nodejs/node/commit/eee522ac29864a55a8bc6686e6b38e93270aa1ca)
+ *
+ * Original from node-domexception by Jimmy Wärting (license: MIT)
+ * https://www.npmjs.com/package/node-domexception
+ */
 function getFromMessageChannel(): DOMExceptionConstructor | undefined {
   try {
     const port = new MessageChannel().port1;
@@ -41,7 +50,10 @@ function getFromMessageChannel(): DOMExceptionConstructor | undefined {
   }
 }
 
-// - Other platforms
+/**
+ * Support:
+ * - All platforms
+ */
 function createPolyfill(): DOMExceptionConstructor {
   // eslint-disable-next-line no-shadow
   const ctor = function DOMException(this: DOMException, message?: string, name?: string) {
