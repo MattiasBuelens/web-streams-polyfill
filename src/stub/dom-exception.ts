ac16720040df1719393b1db563a07055cc142ef8
Mattias Buelens <mattias@buelens.com>
2022-05-24 23:35:56 +0200

    Tweak comments

--- src/stub/dom-exception.ts
+++ src/stub/dom-exception.ts
@@ -21,14 +21,20 @@ function isDOMExceptionConstructor(ctor: unknown): ctor is DOMExceptionConstruct
   }
 }
 
-// - Web browsers
-// - Node 18 and higher (https://github.com/nodejs/node/commit/e4b1fb5e6422c1ff151234bb9de792d45dd88d87)
+/**
+ * Support:
+ * - Web browsers
+ * - Node 18 and higher (https://github.com/nodejs/node/commit/e4b1fb5e6422c1ff151234bb9de792d45dd88d87)
+ */
 function getFromGlobal(): DOMExceptionConstructor | undefined {
   const ctor = globals?.DOMException;
   return isDOMExceptionConstructor(ctor) ? ctor : undefined;
 }
 
-// - Other platforms
+/**
+ * Support:
+ * - All platforms
+ */
 function createPolyfill(): DOMExceptionConstructor {
   // eslint-disable-next-line no-shadow
   const ctor = function DOMException(this: DOMException, message?: string, name?: string) {
